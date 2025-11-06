ac7368b8746c9ee3f25e2251277130d9a1afb587
James <james@spookworkshop.com>
2025-11-06 00:06:35 +0000

    Add host&port params for admin server

--- core/src/vf_core/web_admin/main.py
+++ core/src/vf_core/web_admin/main.py
@@ -3,6 +3,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pathlib import Path
 import uvicorn
+from vf_core.config_manager import ConfigManager
+from vf_core.plugin_manager import PluginManager
 
 from .api import config, plugins
 
@@ -21,16 +23,14 @@ async def root():
     return FileResponse(static_dir / "index.html")
 
 # Run with:
-# uvicorn web_admin.main:app --host 0.0.0.0 --port 8000
+# uvicorn web_admin.main:app --host 127.0.0.1 --port 8000
 # or by calling this function
-async def start_admin_server(config_manager, plugin_manager):
+async def start_admin_server(config_manager: ConfigManager, plugin_manager: PluginManager, host: str = "127.0.0.1", port: int = 8000) -> None:
     app.state.config_manager = config_manager
     app.state.plugin_manager = plugin_manager
 
-    config = uvicorn.Config(app, host="0.0.0.0", port=8000, log_level="info")
+    config = uvicorn.Config(app, host=host, port=port, log_level="info")
     server = uvicorn.Server(config)
     
     server.install_signal_handlers = False
     await server.serve()
-    
-    #uvicorn.run(app, host="0.0.0.0", port=8000)
